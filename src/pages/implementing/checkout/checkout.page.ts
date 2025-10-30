9c542d1b379b3e4f275489a2e2b677d9e917ca18
Tri Nguyen <tri.nguyen@globee.hk>
2025-10-30 18:00:48 +0700

    implementing test-script for checkout page

--- src/pages/implementing/checkout/checkout.page.ts
+++ src/pages/implementing/checkout/checkout.page.ts
@@ -1,28 +1,134 @@
 import { Page, Locator } from "@playwright/test";
 import { BasePage } from "../../base.page";
-import { t } from "../../../../utils/helpers/helpers";
+import { t, PageUtils } from "../../../../utils/helpers/helpers";
 import { Config } from "../../../../config/env.config";
-import { step } from "allure-js-commons";
+import { step, attachment } from "allure-js-commons";
+import { test } from "@playwright/test";
 
 export class CheckoutPage extends BasePage {
     readonly logoImg: Locator;
+    readonly customerDetailsSection: Locator;
+    readonly firstNameTextbox: Locator;
+    readonly lastNameTextbox: Locator;
+    readonly emailTextbox: Locator;
+    readonly phoneTextbox: Locator;
+    readonly continueButton: Locator;
 
     constructor(page: Page) {
         super(page);
         this.logoImg = page.locator('//div[contains(@class,"main-logo-wrapper")]');
+        this.customerDetailsSection = page.locator(`//div[normalize-space(@class)="customer-details-section"]`)
+        this.firstNameTextbox = this.customerDetailsSection.locator(`xpath=.//input[@id="billingFirstName"]`)
+        this.lastNameTextbox = this.customerDetailsSection.locator(`xpath=.//input[@id="billingLastName"]`)
+        this.emailTextbox = this.customerDetailsSection.locator(`xpath=.//input[@id="billingEmail"]`)
+        this.phoneTextbox = this.customerDetailsSection.locator(`xpath=.//input[@id="billingPhoneNumber"]`)
+        this.continueButton = this.customerDetailsSection.locator(`xpath=.//button[@type="submit"]`)
     }
 
     // =========================
     // 🚀 Actions
     // =========================
+    async fillCheckoutYourDetailForm(page: Page, data: CheckoutYourDetailLoad, description?: string): Promise<void> {
+        await step(description || "Fill checkout detail", async () => {
+            if (data.firstName) {
+                await this.type(this.firstNameTextbox, data.firstName,
+                    `Fill firstname textbox: ${data.firstName}`
+                )
+            }
 
+            if (data.lastName) {
+                await this.type(this.lastNameTextbox, data.lastName,
+                    `Fill firstname textbox: ${data.lastName}`
+                )
+            }
+
+            if (data.email) {
+                await this.type(this.emailTextbox, data.email,
+                    `Fill firstname textbox: ${data.email}`
+                )
+            }
+
+            if (data.phone) {
+                await this.type(this.phoneTextbox, data.phone,
+                    `Fill firstname textbox: ${data.phone}`
+                )
+            }
+
+            if (data.newsletter) {
+                await this.clickCheckboxByLabel(page, t.checkoutpage('newsletter'))
+            }
+
+            if (!data.terms) {
+                await this.clickCheckboxByLabel(page, t.checkoutpage('terms'))
+            }
+        })
+    }
 
     // =========================
     // 📦 Helpers
     // =========================
+    async isCheckoutPageDisplayed(): Promise<boolean> {
+        await PageUtils.waitForDomAvailable(this.page)
+        try {
+            const title = await this.page.title();
+            const expectedTitle = t.checkoutpage('title')
+            const currentUrl = await this.page.url();
+            const expectedUrl = Config.baseURL + "checkout?stage=shipping#shipping";
+
+            await test.step("Checkout page data: ", async () => {
+                await attachment("Current Page Title", title, "text/plain");
+                await attachment("Expected Page Title", expectedTitle.toString(), "text/plain");
+                await attachment("Current URL", currentUrl, "text/plain");
+                await attachment("Expected URL", expectedUrl, "text/plain");
+            });
+
+            if (!expectedTitle.includes(title)) {
+                return false;
+            }
+
+            if (!currentUrl.startsWith(expectedUrl)) return false;
+
+            const elementsToCheck = [
+                this.firstNameTextbox,
+                this.lastNameTextbox,
+                this.emailTextbox,
+                this.phoneTextbox,
+                this.continueButton
+            ];
+            for (const locator of elementsToCheck) {
+                if (!locator.isVisible()) {
+                    await step(`Check visibility of element: ${locator.toString()}`, async () => {
+                        console.log(`Element not visible: ${locator.toString()}`);
+                    });
+                    return false;
+                }
+            }
+
+            return true;
+        } catch (error) {
+            console.error('Error checking checkout page:', error);
+            return false;
+        }
+    }
 
     // =========================
     // ✅ Assertions
     // =========================
+    async assertFeedbackMsg(page: Page, label: string, msg: string, description?: string) {
+        await step(description || "Assert invalid feedback under fields", async () => {
+            const invalid_msg = this.customerDetailsSection.locator(`//label[normalize-space(text())="${label}"]/following-sibling::div[@class="invalid-feedback"]`)
+
+            await this.assertText(invalid_msg, msg, `Invalid feedback under ${label} field should be: ${msg}`)
+        })
+    }
 
 }
+
+export type CheckoutYourDetailLoad = {
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    phone?: string;
+    newsletter?: boolean;
+    terms?: boolean;
+};
--- tests/implementing/checkout/checkout.test.ts
+++ tests/implementing/checkout/checkout.test.ts
@@ -1,10 +1,12 @@
 import { test, expect } from "../../../src/fixtures/test-fixture"
-import { t } from "../../../utils/helpers/helpers";
+import { t, clickUntil } from "../../../utils/helpers/helpers";
 import { NewArrivalsPage } from "../../../src/pages/delivery/productlistingpage/newarrivals/newarrivals.page";
 import { createHomePage } from "../../../src/factories/home.factory";
 import { createMinicartPage } from "../../../src/factories/minicart.factory";
 import { createCartPage } from "../../../src/factories/cart.factory";
+import { CheckoutLoginPage } from "../../../src/pages/implementing/checkout/checkoutlogin.page";
 import { step } from "allure-js-commons";
+import { CheckoutPage } from "../../../src/pages/implementing/checkout/checkout.page";
 
 test.describe("Guest checkout", () => {
     test.beforeEach(async ({ basicAuthPage }) => {
@@ -12,6 +14,7 @@ test.describe("Guest checkout", () => {
         const homepage = createHomePage(basicAuthPage)
         const cartpage = createCartPage(basicAuthPage)
         const minicartpage = createMinicartPage(basicAuthPage)
+        const checkoutloginpage = new CheckoutLoginPage(basicAuthPage)
 
         await step('Go to New Arrivals', async () => {
             await homepage.clickMenuItem('newarrivals')
@@ -29,9 +32,111 @@ test.describe("Guest checkout", () => {
             ]);
 
         })
+
+        await step('Go to checkout login page', async () => {
+            await clickUntil(basicAuthPage, homepage.cartIcon, minicartpage.minicartRender, 'visible', {
+                delayMs: 500,
+                maxTries: 3,
+                timeoutMs: 3000
+            })
+
+            await minicartpage.click(minicartpage.checkoutButton,
+                "Click on Checkout button"
+            )
+        })
+
+        await step("Go to guest checkout page", async () => {
+            await checkoutloginpage.click(checkoutloginpage.guestcheckoutButton,
+                "Clicking on Guest checkout button"
+            )
+        })
     });
 
-    test("1. Guest checkout - Checkout screen is displayed", async ({ basicAuthPage }) => {
-        
+    test("1. Guest checkout screen is displayed", async ({ basicAuthPage }) => {
+        const checkoutpage = new CheckoutPage(basicAuthPage)
+
+        await step("Verify the checkout page is displayed", async () => {
+            expect(await checkoutpage.isCheckoutPageDisplayed()).toBe(true)
+        })
+    })
+
+    test("2. Click Continue button without firstname", async ({ basicAuthPage }) => {
+        const checkoutpage = new CheckoutPage(basicAuthPage)
+
+        await step("Fill your detail without firstname", async () => {
+            await checkoutpage.fillCheckoutYourDetailForm(basicAuthPage, withoutFirstName)
+        })
+
+        await checkoutpage.click(checkoutpage.continueButton, "Click on Continue button")
+
+        await checkoutpage.assertFeedbackMsg(basicAuthPage, t.checkoutpage('firstname'), t.checkoutpage('firstnameinvalidmsg'),
+            "Assert invalid msg under Firstname field should be: Please complete this field."
+        )
+    })
+
+    test("3. Click Continue button without lastname", async ({ basicAuthPage }) => {
+        const checkoutpage = new CheckoutPage(basicAuthPage)
+
+        await step("Fill your detail without lastname", async () => {
+            await checkoutpage.fillCheckoutYourDetailForm(basicAuthPage, withoutLastName)
+        })
+
+        await checkoutpage.click(checkoutpage.continueButton, "Click on Continue button")
+
+        await checkoutpage.assertFeedbackMsg(basicAuthPage, t.checkoutpage('lastname'), t.checkoutpage('lastnameinvalidmsg'),
+            "Assert invalid msg under Lastname field should be: Please complete this field."
+        )
+    })
+
+    test("4. Click Continue button without email", async ({ basicAuthPage }) => {
+        const checkoutpage = new CheckoutPage(basicAuthPage)
+
+        await step("Fill your detail without email", async () => {
+            await checkoutpage.fillCheckoutYourDetailForm(basicAuthPage, withoutEmail)
+        })
+
+        await checkoutpage.click(checkoutpage.continueButton, "Click on Continue button")
+
+        await checkoutpage.assertFeedbackMsg(basicAuthPage, t.checkoutpage('email'), t.checkoutpage('emailinvalidmsg'),
+            "Assert invalid msg under Email field should be: Please complete this field."
+        )
+    })
+
+    test("5. Click Continue button without phonenumber", async ({ basicAuthPage }) => {
+        const checkoutpage = new CheckoutPage(basicAuthPage)
+
+        await step("Fill your detail without phonenumber", async () => {
+            await checkoutpage.fillCheckoutYourDetailForm(basicAuthPage, withoutEmail)
+        })
+
+        await checkoutpage.click(checkoutpage.continueButton, "Click on Continue button")
+
+        await checkoutpage.assertFeedbackMsg(basicAuthPage, t.checkoutpage('phone'), t.checkoutpage('phoneinvalidmsg'),
+            "Assert invalid msg under phonenumber field should be: Please complete this field."
+        )
     })
 });
+
+const withoutFirstName = {
+    lastName: "globeeLastName",
+    email: "globeetest@mailinator.com",
+    phone: "335595668",
+    newsletter: true,
+    terms: true
+};
+
+const withoutLastName = {
+    firstName: "globeeFirstName",
+    email: "globeetest@mailinator.com",
+    phone: "335595668",
+    newsletter: true,
+    terms: true
+};
+
+const withoutEmail = {
+    firstName: "globeeFirstName",
+    lastName: "globeeLastName",
+    phone: "335595668",
+    newsletter: true,
+    terms: true
+};
